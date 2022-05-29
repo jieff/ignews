b4896f4d05b66aa4848f678746840228cb301df4
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2022-05-29 15:23:03 -0300

    configurando fonte externa e title dinâmico

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+      <title>ig.news | Home </title>
+      </Head>
+
+      <h1 className={styles.title}>
+      Hello <span>World</span>
+    </h1>
+    
+    </>
+  )
+}
